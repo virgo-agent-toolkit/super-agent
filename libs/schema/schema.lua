d465ddb09990cdd050a4f1a3453a378fb264a7ec
Tim Caswell <tim@creationix.com>
2016-02-04 12:18:10 -0600

    add symlinks and make schema single-file package

--- libs/schema/schema.lua
+++ libs/schema/schema.lua
@@ -1,3 +1,15 @@
+--[[lit-meta
+  name = "creationix/schema"
+  version = "0.1.0"
+  homepage = "https://github.com/virgo-agent-toolkit/super-agent/blob/master/libs/schema"
+  description = "A runtime type-checking system to validate API functions."
+  tags = {"schema", "type", "api"}
+  license = "MIT"
+  contributors = {
+    "Tim Caswell",
+  }
+]]
+
 local concat = table.concat
 
 local function capitalize(name)
