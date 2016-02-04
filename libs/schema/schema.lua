a951330d2a7392b2d4fbb8db755ff8c0c18bbb9d
Tim Caswell <tim@creationix.com>
2016-02-04 17:31:31 -0600

    Publish new version of schema with fixes and new alias feature

--- libs/schema/schema.lua
+++ libs/schema/schema.lua
@@ -1,6 +1,6 @@
 --[[lit-meta
   name = "creationix/schema"
-  version = "0.1.0"
+  version = "0.2.0"
   homepage = "https://github.com/virgo-agent-toolkit/super-agent/blob/master/libs/schema"
   description = "A runtime type-checking system to validate API functions."
   tags = {"schema", "type", "api"}
