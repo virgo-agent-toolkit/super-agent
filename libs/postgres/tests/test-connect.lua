a4d50fe3bbe078ac1001e3a8554b29196895cf7c
Tim Caswell <tim@creationix.com>
2016-02-04 13:01:50 -0600

    Always assert connect in tests

--- libs/postgres/tests/test-connect.lua
+++ libs/postgres/tests/test-connect.lua
@@ -3,10 +3,10 @@ local getenv = require('os').getenv
 local p = require('pretty-print').prettyPrint
 
 coroutine.wrap(function ()
-  local psql = connect {
+  local psql = assert(connect {
     database = getenv("DATABASE") or getenv("USER"),
     password = getenv("PASSWORD")
-  }
+  })
   p(psql)
   p(psql.query("SELECT 'Hello' AS greeting"))
   psql.close()
