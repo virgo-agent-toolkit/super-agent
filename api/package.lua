fe5cef3b85e94e91bec49956f5b24c990d7e4a79
Tim Caswell <tim@creationix.com>
2016-02-09 17:02:17 -0600

    Add metadata for lit make

--- api/package.lua
+++ api/package.lua
@@ -1,6 +1,18 @@
 return {
-  name = "super-agent/api",
+  name = "super-agent/api-server",
+  private = true,
   version = "0.0.1",
+  luvi = {
+    version = "2.6.1",
+    flavor = "tiny",
+  },
+  files = {
+    "main.lua",
+    "server.lua",
+    "package.lua",
+    "libs/**.lua",
+    "crud/**.lua",
+  },
   dependencies = {
     "creationix/weblit-app",
     "creationix/weblit-websocket",
