a94f409188951318900784c1348bfb76b2ce814a
Tim Caswell <tim@creationix.com>
2016-02-09 16:02:15 -0600

    Record overheads on OSX

--- api/main.lua
+++ api/main.lua
@@ -17,3 +17,10 @@
 dofile("luvit-loader.lua")
 require('./server')
 require('uv').run()
+
+-- Memory comparisions running server on different platforms
+
+-- On OSX
+-- luvit(v2.9.1) 6.5MB
+-- luajit(v2.0.4-homebrew) 5.0MB
+-- lua(v5.2.4-homebrew) 12.3 MB
