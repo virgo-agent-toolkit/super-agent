f769ee633799405feb6967e22df86264b0c23463
Tim Caswell <tim@creationix.com>
2016-02-09 15:56:21 -0600

    Add instructions for main.lua

--- api/main.lua
+++ api/main.lua
@@ -1,3 +1,19 @@
+-- This is a stub that allows running server.lua using normal lua or luajit
+-- For PUC lua:
+--
+--   luarocks luv luabitop
+--   lua main.lua
+--
+-- For luajit:
+--
+-- build luv from source and copy `luv.so` to package.cpath
+--
+--  luajit main.lua
+--
+-- For luvit: (skip this file)
+--
+--   luvit server.lua
+--
 dofile("luvit-loader.lua")
 require('./server')
 require('uv').run()
