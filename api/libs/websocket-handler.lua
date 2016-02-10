bad91a0325d254dcdc4dfc5f821352dd7092f67b
Tim Caswell <tim@creationix.com>
2016-02-09 18:24:07 -0600

    Implement request/response for websocket server

--- api/libs/websocket-handler.lua
+++ api/libs/websocket-handler.lua
@@ -1,21 +1,94 @@
 local call = require('registry').call
+local jsonDecode = require('json').parse
+local jsonEncode = require('json').stringify
+local msgpackDecode = require('msgpack').decode
+local msgpackEncode = require('msgpack').encode
+local p = require('pretty-print').prettyPrint
+
+local function isArray(value)
+  if type(value) ~= "table" then return end
+  local i = 1
+  for k in pairs(value) do
+    if i ~= k then return end
+    i = i + 1
+  end
+  return true
+end
 
 -- Incoming requests are in the form:
 -- [id, name, args...]
 -- Outgoing responses are in the form:
 -- [-id, result]
--- Outgoing errors are:
--- [0, err, id?]
 -- Outgoing stream chunks are in the form:
 -- [sid, chunk]
 -- Incoming stream chunks are in the form:
 -- [-sid, chunk]
--- End of stream messages are
--- [sid] or [-sid]
+-- errors are in the format:
+-- [0, err, (s)id?]
+local function handleMessage(message, send, peerName)
+  local id = message[1]
+  if type(id) ~= "number" then
+    return send(0, "Invalid message format")
+  end
+  if id == 0 then
+    return p("Error message", unpack(message))
+  end
+  if id > 0 then
+    local name = message[2]
+    if type(name) ~= "string" then
+      return send(0, "Request name must be string", id)
+    end
+    print("REQUEST " .. name .. " " .. peerName)
+    local args = {}
+    for i = 3, #message do
+      args[i - 2] = message[i]
+    end
+    local result, err = call(name, args)
+    if result then
+      return send(-id, result)
+    end
+    return send(0, err, id)
+  end
+  error("TODO: handle stream packets from client")
+end
+
+
 return function (req, read, write)
-  p(req.headers)
+  local peer = req.socket:getpeername()
+  local peerName = peer.ip .. ":" .. peer.port
+  local userAgent = req.headers["user-agent"]
+  if userAgent then
+    peerName = peerName .. ' ' .. userAgent
+  end
+  print("UPGRADE " .. peerName)
+  local encode = jsonEncode
+  local function send(...)
+    return write {
+      opcode = encode == jsonEncode and 1 or 2,
+      payload = encode {...}
+    }
+  end
+
   for frame in read do
-    p(frame)
+    local message
+    if frame.opcode == 1 then
+      message = jsonDecode(frame.payload)
+      encode = jsonEncode
+    elseif frame.opcode == 2 then
+      message = msgpackDecode(frame.payload)
+      encode = msgpackEncode
+    else
+      send(0, "Unexpected frame type: " .. frame.opcode)
+    end
+    if message then
+      if isArray(message) then
+        handleMessage(message, send, peerName)
+      else
+        send(0, "Invalid message format")
+      end
+    end
   end
   write()
+  print("DISCONNECT " .. peerName)
+
 end
--- api/test-server.lua
+++ api/test-server.lua
@@ -2,7 +2,7 @@ local p = require('pretty-print').prettyPrint
 local request = require('coro-http').request
 local msgpackEncode = require('msgpack').encode
 local jsonEncode = require('json').stringify
-
+local connect = require('websocket-client')
 
 local function dump(...)
   local args = {...}
@@ -10,17 +10,33 @@ local function dump(...)
     p(args[i])
   end
 end
+local userAgent = "test-server.lua"
 
 coroutine.wrap(function ()
   local aep = {
     hostname = "localhost"
   }
   dump(request("POST", "http://localhost:8080/api/aep.create", {
-    {"User-Agent", "test-server.lua"},
+    {"User-Agent", userAgent},
     {"Content-Type", "application/json"}
   }, jsonEncode{aep}))
   dump(request("POST", "http://localhost:8080/api/aep.create", {
-    {"User-Agent", "test-server.lua"},
+    {"User-Agent", userAgent},
     {"Content-Type", "application/msgpack"}
   }, msgpackEncode{aep}))
+  local read, write = connect("ws://localhost:8080/websocket", "schema-rpc", {
+    {"User-Agent", userAgent}
+  })
+  write {
+    opcode=1,
+    payload=jsonEncode{1,"aep.create",aep}
+  }
+  dump("Result", read())
+  write {
+    opcode=2,
+    payload=msgpackEncode{1,"aep.create",aep}
+  }
+  dump("result", read())
+  write()
+
 end)()
