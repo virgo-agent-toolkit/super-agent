a8c8ca32e9080f873637bf08926e57a10324d1e2
Tim Caswell <tim@creationix.com>
2016-03-08 21:26:01 -0600

    Add diskusage

--- agent/README.md
+++ agent/README.md
@@ -4,7 +4,7 @@
 This will be a single executable built using luvi with luajit and openssl
 embedded.
 
-- The agent reads its local config to fund list of AEPs to connect to.
+- The agent reads its local config to find list of AEPs to connect to.
 - It also sets some global constraint overrides based on the local config.
 - It connects to multiple concurrently.
   - Commands can come in from any AEP, but when sending queries, use the lowest
--- agent/main.lua
+++ agent/main.lua
@@ -14,15 +14,15 @@ local register = registry.register
 local String = registry.String
 local Function = registry.Function
 local Optional = registry.Optional
-local Boolean = registry.Boolean
+local Int = registry.Int
 
 assert(register("scandir", "Reads a directory, calling onEntry for each name/type pair", {
   {"path", String},
   {"onEntry", Function},
-}, {Boolean, Optional(String)}, function (path, onEntry)
-  local req, err = fs.scandir(path)
-  if not req then return {false, err} end
-  for entry in req do
+}, Optional(String), function (path, onEntry)
+  local iter, err = fs.scandir(path)
+  if not iter then return {false, err} end
+  for entry in iter do
     onEntry(entry.name, entry.type)
   end
   return {true}
@@ -35,15 +35,56 @@ assert(register("echo", "Echo testing streams", {
   return echo
 end))
 
+-- diskusage(
+--   path: String,
+--   depth: Integer,
+--   onEntry: Callback(path: String, size: Integer)
+-- ) -> error: Optional(String)
+assert(register("diskusage", "Calculate diskusage of folders and subfolders", {
+  {"path", String},
+  {"maxDepth", Int},
+  {"onEntry", Function}
+}, Optional(String), function (rootPath, maxDepth, onEntry)
+  local function scan(path, depth)
+    local stat, err = fs.stat(path)
+    if not stat then
+      return nil, err
+    end
+    local total = stat.size
+    if stat.type == "directory" then
+      local iter, err2 = fs.scandir(path)
+      if not iter then return nil, err2 end
+      for entry in iter do
+        local subpath = (path == "/" and "" or path) .. "/" .. entry.name
+        local subtotal, err3 = scan(subpath, depth - 1)
+        if not subtotal then return nil, err3 end
+        total = total + subtotal
+      end
+    end
+    if depth >= 0 then
+      onEntry(path, total)
+    end
+    return total
+  end
+  local _, err = scan(rootPath, maxDepth)
+  return err
+end))
 
--- assert(register("pty", "Create a pty with given shell and dimensions, uses streams", {
---   {"shell", String},
---   {"cols", Int},
---   {"rows", Int},
---   {"onExit", Int},
---   {"onStdout", Int},
---   {"onStderr", Int},
--- }, Int, require('pty')))
+
+-- pty(
+--   shell: String,
+--   uid: Integer,
+--   gid: Integer,
+--   cols: Integer,
+--   rows: Integer,
+--   onTitle: Function(title: String),
+--   onOut: Function(chunk: Buffer),
+--   onClose: Function()
+-- ) -> error: Optional(String),
+--      write: Function(chunk: Buffer),
+--      close: Function(error: Optional(String)),
+--      resize: Function(cols: Integer, rows: Integer)
+
 
 local function log(...)
   p("log", ...)
